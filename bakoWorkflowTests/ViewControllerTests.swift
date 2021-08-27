d1c27abb6681ce9fc213e9c1cf7209c139d323ce
bakoAbdullah <bako.abdullah.24@gmail.com>
2021-08-27 19:24:38 +0300

    ok

--- bakoWorkflowTests/ViewControllerTests.swift
+++ bakoWorkflowTests/ViewControllerTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import bakoWorkflow
 
 class ViewControllerTests: XCTestCase {
-  
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -19,7 +19,7 @@ class ViewControllerTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
+        // This is an example of a function  al test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
