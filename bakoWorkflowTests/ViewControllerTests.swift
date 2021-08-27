42be38abcc940d9e1864857ec5a8716aa5fd190a
bakoAbdullah <bako.abdullah.24@gmail.com>
2021-08-27 20:26:26 +0300

    test the failing tests

--- bakoWorkflowTests/ViewControllerTests.swift
+++ bakoWorkflowTests/ViewControllerTests.swift
@@ -25,7 +25,7 @@ class ViewControllerTests: XCTestCase {
 
     func test_string() {
         let string = "test"
-        XCTAssertEqual(string, "test333")
+        XCTAssertEqual(string, "test33")
     }
 
     func test_bool() {
