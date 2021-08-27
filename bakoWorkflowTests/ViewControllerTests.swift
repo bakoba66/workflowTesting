7bd2a332eff806d6e1e3678862926e3867d556a3
bakoAbdullah <bako.abdullah.24@gmail.com>
2021-08-27 16:00:52 +0300

    commit

---
+++ bakoWorkflowTests/ViewControllerTests.swift
@@ -0,0 +1,36 @@
+//
+//  ViewControllerTests.swift
+//  bakoWorkflowTests
+//
+//  Created by bako on 27/08/2021.
+//
+
+import XCTest
+@testable import bakoWorkflow
+
+class ViewControllerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func test_string() {
+        let string = "test"
+        XCTAssertEqual(string, "test")
+    }
+
+    func test_bool() {
+        let bool = true
+        XCTAssertTrue(bool)
+    }
+}
+
